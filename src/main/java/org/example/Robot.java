1cd0620e3f88f3d2613fd8c264ab57a7929d1be3
unknown <greateducation123@yahoo.com>
2023-02-22 14:44:52 -0600

    Moved git and gitignore into the IdeaProjects folder with pom.xml

---
+++ src/main/java/org/example/Robot.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Robot {
+
+    //Instance variables (HAS A)
+    public String name;
+    public String weaponName;
+    public int weaponPower;
+
+    //Constructor
+    public Robot() {
+    }
+}
